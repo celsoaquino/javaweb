84eadd5f8b9d2de8b27bf32724b91e2d4d4ae7ba
celsoaquino <celso.aquino@ymail.com>
2019-02-18 21:14:00 -0300

    Criado operacao de instaciacao

--- src/main/java/com/celsoaquino/javaweb/JavawebApplication.java
+++ src/main/java/com/celsoaquino/javaweb/JavawebApplication.java
@@ -1,13 +1,29 @@
  package com.celsoaquino.javaweb;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class JavawebApplication {
+import com.celsoaquino.javaweb.domain.Categoria;
+import com.celsoaquino.javaweb.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class JavawebApplication implements CommandLineRunner{
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(JavawebApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática"); 
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
 	}
 
 }
